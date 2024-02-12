e703d358ac53fd786570b90d84aa06e1d63dd206
NaderMR <46198349+NaderMR@users.noreply.github.com>
2024-02-12 18:44:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,8 +24,6 @@ def meteo():
         dt_value = list_element.get('dt')
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
 
 @app.route("/rapport/")
 def mongraphique():
