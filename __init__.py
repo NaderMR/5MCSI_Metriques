7565384c600ac3a26cce43f2286b3aa3c6751705
NaderMR <46198349+NaderMR@users.noreply.github.com>
2024-02-12 18:28:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,15 +14,15 @@ def hello_world():
 @app.route("/contact/")
 def MaPremiereAPI():
     return render_template("contact.html")
-@app.route('/paris/')
+@app.route("/paris/")
 def meteo():
-    response = urlopen('samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
